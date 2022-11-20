1426945a8efc49cb4b97e2562a00eb475d405b70
Johan Eggers <j.eggers@eckelmann.de>
2022-11-20 11:41:29 +0100

    Unit-Test für user.service

--- api-connector-spa/src/app/user.service.spec.ts
+++ api-connector-spa/src/app/user.service.spec.ts
@@ -1,19 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {CountriesDTO, UserService} from './user.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should request users', () => {
+
+    const countriesWithUserData: CountriesDTO = {
+      countries: [
+        {name: "Sweden", users: [{gender: "female", name: "Anne", country: "Belgium", email: "a@b.com"}]}
+      ]
+    };
+
+    service.getUsers().subscribe(countries => {
+      expect(countries).toEqual(countriesWithUserData);
+    });
+
+    const fakeRequest = httpMock.expectOne({ url: 'http://localhost:12080/users', method: 'GET' });
+    fakeRequest.flush(countriesWithUserData, { status: 200, statusText: 'OK' });
+    httpMock.verify();
   });
 });
